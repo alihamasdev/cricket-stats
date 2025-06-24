0468da834f561a19f545ea1bea51401af285fd61
alihamasdev <alihamasdev@gmail.com>
2025-06-24 20:56:12 +0500

    add theme provider, toggler components for dark mode support and header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist } from "next/font/google";
 
 import "./globals.css";
 
+import { ThemeProvider, ThemeToggler } from "@/components/theme";
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"]
@@ -15,9 +17,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className="antialiased" style={geistSans.style}>
-				{children}
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<main className="container mx-auto flex min-h-dvh w-full flex-col px-4">
+						<header className="flex w-full items-center justify-between py-4">
+							<h1 className="text-2xl font-semibold">Cricket Stats</h1>
+							<ThemeToggler />
+						</header>
+						{children}
+					</main>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
