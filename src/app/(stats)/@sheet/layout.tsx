ee758b7680352e14cba3c14ed93f07fe3a94e7e0
hello-world-2166 <alihamasgh@gmail.com>
2025-06-27 08:45:29 +0500

    feat: add default and layout components for stats sheet

---
+++ src/app/(stats)/@sheet/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+
+export default function SheetLayout({ children }: { children: React.ReactNode }) {
+	const router = useRouter();
+
+	return (
+		<Sheet open={true} onOpenChange={() => router.back()}>
+			<SheetContent>
+				<SheetHeader className="flex-row justify-between">
+					<SheetTitle className="text-2xl">Add Stats</SheetTitle>
+					<SheetDescription />
+					<SheetClose asChild>
+						<Button variant="outline" size="icon" className="size-8">
+							<X />
+						</Button>
+					</SheetClose>
+				</SheetHeader>
+				{children}
+			</SheetContent>
+		</Sheet>
+	);
+}
