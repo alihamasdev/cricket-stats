5345a7da04c24c70d1e1fdda6ca17ee2d6385864
alihamasdev <alihamasdev@gmail.com>
2025-08-26 22:51:22 +0500

    fix: awap out for players

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -28,8 +28,8 @@ export default function ComparePage() {
 		},
 		{ history: "push" }
 	);
-	const player1Stats = getComparePlayerStats(player1, player2, date);
-	const player2Stats = getComparePlayerStats(player2, player1, date);
+	const { outs: player2_Outs, ...player1Stats } = getComparePlayerStats(player1, player2, date);
+	const { outs: player1_Outs, ...player2Stats } = getComparePlayerStats(player2, player1, date);
 
 	return (
 		<div className="mb-auto w-full max-w-100 space-y-6">
@@ -44,7 +44,7 @@ export default function ComparePage() {
 				<PlayerNameField label="Player 2" value={player2} onSelect={(value) => setSearchParams((prev) => ({ ...prev, player2: value }))} />
 			</div>
 			<div className="grid w-full grid-cols-[1.3fr_1.2fr_1.3fr] border p-6 md:grid-cols-[1.4fr_1.1fr_1.4fr]">
-				<SingleComparePlayer name={player1} stats={player1Stats} />
+				<SingleComparePlayer name={player1} outs={player1_Outs} {...player1Stats} />
 				<div className="pt-17 text-center md:pt-22">
 					<p className="text-muted-foreground mb-4 text-base/7">vs</p>
 					<div className="grid grid-rows-6 gap-y-2 text-sm font-medium md:text-base">
@@ -56,7 +56,7 @@ export default function ComparePage() {
 						<p>Outs</p>
 					</div>
 				</div>
-				<SingleComparePlayer name={player2} stats={player2Stats} />
+				<SingleComparePlayer name={player2} outs={player2_Outs} {...player2Stats} />
 			</div>
 		</div>
 	);
@@ -64,22 +64,26 @@ export default function ComparePage() {
 
 interface SingleComparePlayerProps {
 	name: string;
-	stats: { runs: number; balls: number; fours: number; sixes: number; outs: number };
+	runs: number;
+	balls: number;
+	fours: number;
+	sixes: number;
+	outs: number;
 }
 
-function SingleComparePlayer({ name, stats }: SingleComparePlayerProps) {
-	const strikeRate = (stats.runs / stats.balls) * 100;
+function SingleComparePlayer({ name, runs, balls, fours, sixes, outs }: SingleComparePlayerProps) {
+	const strikeRate = (runs / balls) * 100;
 	return (
 		<div className="flex flex-col items-center space-y-2">
 			<PlayerAvatar name={name} className="size-15 md:size-20" />
 			<h2 className="text-center text-xl/7 font-semibold capitalize">{name || "Player"}</h2>
 			<div className="grid grid-rows-6 gap-y-2 pt-2 text-center text-sm md:text-base">
-				<p>{stats.runs}</p>
-				<p>{stats.balls}</p>
-				<p>{stats.fours}</p>
-				<p>{stats.sixes}</p>
+				<p>{runs}</p>
+				<p>{balls}</p>
+				<p>{fours}</p>
+				<p>{sixes}</p>
 				<p>{strikeRate ? strikeRate.toFixed(2) : "0.00"}</p>
-				<p>{stats.outs}</p>
+				<p>{outs}</p>
 			</div>
 		</div>
 	);
